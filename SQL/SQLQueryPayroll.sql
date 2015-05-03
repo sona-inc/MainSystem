e7020b3602bd37771f96120675d7a4bafab6bae1
Sambath <sambathc98@gmail.com>
2015-05-03 12:14:51 +0700

    Days changed to tinyint instead of ?

--- SQL/SQLQueryPayroll.sql
+++ SQL/SQLQueryPayroll.sql
@@ -1,1 +1,1 @@
-Create Table tbPayroll(ID int identity primary key,WorkerID int,Days?,PayrolledDate Date,PaymentType varchar(30),Amount money,TotalAmount money,Constraint FKWorkerID Foreign Key(WorkerID) references tbWorker(WorkerID) On Delete Cascade on Update Cascade)
+Create Table tbPayroll(ID int identity primary key,WorkerID int,Days tinyint,PayrolledDate Date,PaymentType varchar(30),Amount money,TotalAmount money,Constraint FKPayrollWorkerID Foreign Key(WorkerID) references tbWorker(WorkerID) On Delete Cascade on Update Cascade)
