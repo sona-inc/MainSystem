ddde6e854a3d42bf3010644d42843988aa36bf4e
Phat sovathana <sovathana.phat@gmail.com>
2015-05-06 00:20:46 +0700

    SqlCms new Version

---
+++ Databases/sqlCms/sqlCms/SampleClass.vb
@@ -0,0 +1,280 @@
+﻿Imports System.Collections.Generic
+Imports System.Data
+Imports System.Linq
+Imports System.Reflection
+
+Namespace sqlCms
+    Public Class SampleClass
+        Private db As Systemdb
+
+        ' The table
+        Private table As String
+
+        ' The primary key of the table
+        Private pk As String
+
+        ' The prefix used to distinguish certain properties as columns
+        Protected c_prefix As Char = "_"c
+
+        Public WriteOnly Property table_() As String
+            Set(value As String)
+                Me.table = value
+            End Set
+        End Property
+
+        Public WriteOnly Property pk_() As String
+            Set(value As String)
+                Me.pk = value
+            End Set
+        End Property
+
+        Private internProperties As List(Of String)
+
+        Public Properties As Dictionary(Of String, String)
+
+        Public Sub New()
+            db = New Systemdb()
+            Properties = New Dictionary(Of String, String)()
+            internProperties = New List(Of String)()
+        End Sub
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="id"></param>
+        ''' <returns></returns>
+        Public Function find(Optional id As Integer = 0) As SampleClass
+            If id <> 0 Then
+                Dim sql As String = (Convert.ToString((Convert.ToString("SELECT * FROM ") & Me.table) + " WHERE ") & Me.pk) + "= @pk LIMIT 1"
+
+                Dim d As DataTable = Me.db.query(sql, New String() {"pk", id.ToString()})
+
+                Dim row As New Dictionary(Of String, String)()
+
+                If d.Rows.Count > 0 Then
+                    For i As Integer = 0 To d.Columns.Count - 1
+                        row.Add(Me.c_prefix + d.Columns(i).ColumnName.ToLower(), d.Rows(0)(i).ToString())
+                    Next
+                End If
+
+                For Each pi As PropertyInfo In Me.[GetType]().GetProperties()
+                    Dim propertyName As String = pi.Name
+
+                    If pi IsNot Nothing AndAlso pi.CanWrite Then
+                        If row.ContainsKey(propertyName) Then
+                            If pi.PropertyType = GetType(String) Then
+                                pi.SetValue(Me, row(propertyName))
+                            End If
+                            If pi.PropertyType = GetType(Integer) Then
+                                pi.SetValue(Me, Integer.Parse(row(propertyName)))
+                            End If
+                        End If
+                    End If
+                Next
+
+                Me.internProperties.Clear()
+                Me.Properties.Clear()
+            End If
+
+            Return Me
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="id"></param>
+        ''' <returns>Number of affacted rows</returns>
+        Public Function save(Optional id As Integer = 0) As Integer
+            bindProperties(Me)
+
+            If Me.Properties.Count > 0 Then
+                If id <> 0 Then
+                    Me.Properties(Me.pk) = id.ToString()
+                End If
+
+                Dim update As String = String.Empty
+
+                For Each column As String In Me.Properties.Keys
+                    If column <> Me.pk Then
+                        update += (Convert.ToString(column & Convert.ToString(" =@")) & column) + " , "
+                    End If
+                Next
+
+                update = update.Trim()
+                update = update.Substring(0, update.Length - 1)
+
+                Dim sql As String = (Convert.ToString((Convert.ToString((Convert.ToString("UPDATE ") & Me.table) + " SET ") & update) + " WHERE ") & Me.pk) + "= @pk LIMIT 1"
+
+                Dim pk_index_intern As Integer = Me.internProperties.IndexOf(Me.pk)
+
+                Me.internProperties.RemoveAt(pk_index_intern)
+                Me.internProperties.RemoveAt(pk_index_intern)
+
+                Me.internProperties.Add("@pk")
+                Me.internProperties.Add(Me.Properties(Me.pk))
+
+                Return Me.db.nQuery(sql, Me.internProperties.ToArray())
+            End If
+
+            Return 0
+        End Function
+
+        ''' <summary>
+        ''' </summary>
+        ''' <param name="id"></param>
+        ''' <returns>Number of affacted rows</returns>
+        Public Function delete(Optional id As Integer = 0) As Integer
+            If id > 0 Then
+
+                Dim sql As String = (Convert.ToString((Convert.ToString("DELETE FROM ") & Me.table) + " WHERE ") & Me.pk) + "= @pk LIMIT 1"
+                Return Me.db.nQuery(sql, New String() {"pk", id.ToString()})
+            End If
+
+            Return 0
+        End Function
+
+        ''' <summary>
+        ''' To-do : test
+        ''' </summary>
+        ''' <returns>Number of affacted rows</returns>
+        Public Function create() As Integer
+            If Me.Properties.Count = 0 Then
+                bindProperties(Me)
+            End If
+
+            If Me.Properties.Count > 0 Then
+                ' The column names
+                Dim fields As List(Of String) = Me.Properties.Keys.ToList()
+
+                ' The value of the columns
+                Dim fieldsvals As List(Of String) = Me.Properties.Values.ToList()
+
+                Dim pk_index As Integer = fields.IndexOf(Me.pk)
+                Dim pk_index_intern As Integer = Me.internProperties.IndexOf(Me.pk)
+
+                ' Remove the primary key, we don't need that in an insert query
+                fields.RemoveAt(pk_index)
+                fieldsvals.RemoveAt(pk_index)
+
+                Me.internProperties.RemoveAt(pk_index_intern)
+                Me.internProperties.RemoveAt(pk_index_intern)
+
+                Dim sFields As String = [String].Join(",", fields)
+
+                For i As Integer = 0 To fieldsvals.Count - 1
+                    fields(i) = "@" + fields(i)
+                Next
+
+                Dim sql As String = (Convert.ToString((Convert.ToString("INSERT INTO ") & Me.table) + "(") & sFields) + ")" + " VALUES(" + [String].Join(",", fields) + ")"
+
+                Dim result As Integer = Me.db.nQuery(sql, Me.internProperties.ToArray())
+
+                Me.Properties.Clear()
+                Me.internProperties.Clear()
+
+                Return result
+            Else
+                Dim sql As String = (Convert.ToString("INSERT INTO ") & Me.table) + "()" + " VALUES()"
+                Return Me.db.nQuery(sql)
+            End If
+        End Function
+
+        ''' <summary>
+        ''' To-do: test
+        ''' </summary>
+        ''' <param name="o"></param>
+        ''' <returns>Number of affacted rows</returns>
+        Public Function create(obj As Object()) As Integer
+
+            Dim rows_affected As Integer = 0
+
+            For Each o As Object In obj
+                bindProperties(o)
+                rows_affected += Me.create()
+            Next
+
+            Return rows_affected
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="obj"></param>
+        ''' <returns></returns>
+        Public Function create(obj As List(Of Object)) As Integer
+            Dim rows_affected As Integer = 0
+
+            For Each o As Object In obj
+                bindProperties(o)
+                rows_affected += Me.create()
+            Next
+
+            Return rows_affected
+        End Function
+
+        ''' <summary>
+        ''' </summary>
+        ''' <returns>Datatable</returns>
+        Public Function all() As DataTable
+            Return Me.db.table(Me.table)
+        End Function
+
+
+        ' Aggregates methods
+
+        Private Function aggregateInit(sql As String) As Double
+            Dim value As String = db.[single](sql)
+
+            Dim result As Double
+
+            If value IsNot Nothing AndAlso Double.TryParse(value, result) Then
+                Return Double.Parse(value)
+            End If
+
+            Return 0
+        End Function
+
+        Public Function min(field As String) As Double
+            Return aggregateInit(Convert.ToString((Convert.ToString("SELECT min(") & field) + ")" + " FROM ") & Me.table)
+        End Function
+
+        Public Function max(field As String) As Double
+            Return aggregateInit(Convert.ToString((Convert.ToString("SELECT max(") & field) + ")" + " FROM ") & Me.table)
+        End Function
+
+        Public Function avg(field As String) As Double
+            Return [Double].Parse(aggregateInit(Convert.ToString((Convert.ToString("SELECT avg(") & field) + ")" + " FROM ") & Me.table).ToString())
+        End Function
+
+        Public Function sum(field As String) As Double
+            Return aggregateInit(Convert.ToString((Convert.ToString("SELECT sum(") & field) + ")" + " FROM ") & Me.table)
+        End Function
+
+        Public Function count(field As String, Optional limit As Integer = 0) As Double
+            If limit > 0 Then
+                Return aggregateInit((Convert.ToString((Convert.ToString("SELECT count(") & field) + ")" + " FROM ") & Me.table) + " LIMIT " + limit.ToString())
+            End If
+
+            Return aggregateInit(Convert.ToString((Convert.ToString("SELECT count(") & field) + ")" + " FROM ") & Me.table)
+        End Function
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="o"></param>
+        Private Sub bindProperties(o As Object)
+            For Each pi As PropertyInfo In o.[GetType]().GetProperties()
+                ' Table column prefix
+                If pi.Name(0) = Me.c_prefix Then
+                    Dim [property] As Object = pi.GetValue(o, Nothing)
+                    If [property] IsNot Nothing Then
+                        Me.Properties(pi.Name.Substring(1)) = [property].ToString()
+
+                        Me.internProperties.Add(pi.Name.Substring(1))
+                        Me.internProperties.Add([property].ToString())
+                    End If
+                End If
+            Next
+        End Sub
+    End Class
+End Namespace
--- Databases/sqlCms/sqlCms/Systemdb.vb
+++ Databases/sqlCms/sqlCms/Systemdb.vb
@@ -11,9 +11,20 @@ Imports System.Threading
 Imports System
 Imports System.Data.OleDb
 Imports System.Data.SqlClient
+Imports System.Text
+Imports System.Windows
+Imports System.IO
+Imports System.Linq
+Imports System.Web
+Imports Microsoft.SqlServer, System.Management
+
+
+
+
 Namespace sqlCms
     Public Class Systemdb
-        'Error Line 10 Please help me !!
+        Public str As String
+        Public filePaths As String() = Directory.GetFiles(Directory.GetCurrentDirectory + "\dumps\", "*.sql")
         Public connect As SqlConnection
         Public command As New SqlCommand
         Public rder As SqlDataReader
@@ -23,6 +34,10 @@ Namespace sqlCms
         Public affected_rows As Integer
         Public squery As String
         Public parameters As List(Of String)
+        Public affare As Integer
+        Public stype As String
+
+
 
         Public Sub New()
             connection()
@@ -57,14 +72,98 @@ Namespace sqlCms
             connect.Close()
             connect.Dispose()
         End Sub
+        Public Sub Sqldumper()
 
-        Private Sub Init(Query As String, Optional sonaData As String() = Nothing)
+
+            Dim fils As New StringBuilder()
+            Try
+                For i As Integer = 0 To filePaths.GetUpperBound(0)
+                    Dim script As String = File.ReadAllText(filePaths(i))
+                    str = fils.AppendLine(script).ToString
+                Next
+            Catch ex As Exception
+
+            End Try
+
+            Using connection As New SqlConnection(sqlCms.Cons.sqlConnect)
+                connection.Open()
+                Using command As SqlCommand = New SqlCommand(str, connection)
+                    Try
+                        command.ExecuteNonQuery()
+                    Catch ex As Exception
+
+                    End Try
+
+                End Using
+            End Using
+
+
+        End Sub
+        Public Sub CreateStoreProcedure(sql As String, Optional parameter As String = Nothing, Optional name As String = Nothing)
+
+            Dim query As String = "select * from sysobjects where type='P' and name='" & name & "'"
+            Dim spExists As Boolean = False
+            Dim sbSP As New StringBuilder()
+
+
+            If bConnected = False Then
+                connection()
+            End If
+            Using connection As New SqlConnection(sqlCms.Cons.sqlConnect)
+                Try
+                    connection.Open()
+                    Console.WriteLine("ServerVersion: {0}", connection.ServerVersion)
+                    Console.WriteLine("Database: {0}", connection.Database)
+
+                    'connection.ChangeDatabase("tbAdminSchema")
+                    Console.WriteLine("Database: {0}", connection.Database)
+                Catch ex As Exception
+
+                End Try
+                Using command As SqlCommand = New SqlCommand(query, connection)
+
+
+                    Using rder = command.ExecuteReader()
+                        While rder.Read()
+                            spExists = True
+                            Exit While
+                        End While
+                    End Using
+
+                End Using
+                If spExists = True Then
+                Else
+                    sbSP.AppendLine(Convert.ToString("Create Procedure [" & name & "]" & Convert.ToString(parameter) & Space(1) & " as" & Space(1) & "" & sql & ""))
+                    Me.stype = sql.ToLower()
+                    If stype.Contains("update") OrElse stype.Contains("insert") OrElse stype.Contains("delete") Then
+                        Using cmd As New SqlCommand(sbSP.ToString(), connection)
+                            cmd.CommandType = CommandType.Text
+                            cmd.ExecuteNonQuery()
+                            connect.Close()
+                        End Using
+
+                    ElseIf stype.Contains("select") Then
+                        Using cmd As New SqlCommand(sbSP.ToString(), connection)
+                            cmd.CommandType = CommandType.Text
+                            cmd.ExecuteReader()
+                            connect.Close()
+                        End Using
+                    End If
+
+                End If
+
+            End Using
+
+        End Sub
+        Private Sub Init(Query As String, Optional sonaData As String() = Nothing, Optional isbinary As Boolean = False, Optional bin As String = Nothing, Optional filebyte As Byte() = Nothing)
             ' Make connection if it's not connect yet.
             If bConnected = False Then
                 connection()
             End If
 
-            ' Automatically disposes the SQL Servercommand instance easy for use as offline 
+            ' Automatically disposes the SQL Servercommand instance easy for use as offline
+
+
             Using command = New SqlCommand(Query, connect)
 
                 ' 
@@ -94,10 +193,13 @@ Namespace sqlCms
                         Debug(exception)
                     End Try
                     Me.tb = dt
+
                 End If
                 If squery.Contains("delete") OrElse squery.Contains("update") OrElse squery.Contains("insert") Then
+
                     Try
                         affected = command.ExecuteNonQuery()
+
                     Catch my As SqlException
                         Dim exception As String = Convert.ToString("Exception : " + my.Message.ToString() + vbLf & vbCr & " SQL Query : " & vbLf & vbCr) & squery
 
@@ -105,24 +207,31 @@ Namespace sqlCms
 
                         Debug(exception)
                     End Try
+                    If isbinary = True Then
+                        With command.Parameters
+                            .Add(bin, SqlDbType.VarBinary, -1).Value = filebyte
+                        End With
 
+                        command.ExecuteNonQuery()
+                        command.Dispose()
+                    End If
                     Me.affected_rows = affected
                 End If
-                If squery.Contains("Exc") Then
-                    Try
-                        command.CommandType = CommandType.StoredProcedure
-                    Catch ex As Exception
-                        Dim exception As String = Convert.ToString("Exception : " + squery)
-                        Console.WriteLine(exception, "Uncaught Sql Exception")
-                    End Try
-
-                End If
-
-                Me.parameters.Clear()
             End Using
+            If Query.Contains("InsertAdminSchema") Then
+
+            End If
+
+            Me.parameters.Clear()
+
+
         End Sub
 
         Public Sub sona_SetData(field As String, value As String)
+            parameters.Add(Convert.ToString((Convert.ToString("@") & field) + ChrW(127)) & value)
+        End Sub
+        Public parametDate As New List(Of Date)
+        Public Sub sona_SetData(field As String, value As Date)
             parameters.Add(Convert.ToString((Convert.ToString("@") & field) + ChrW(127)) & value)
         End Sub
 
@@ -179,10 +288,121 @@ Namespace sqlCms
             Init(Convert.ToString("SELECT * FROM ") & tb, sonaData)
             Return Me.tb
         End Function
-        Public Function Exc(store As String, Optional stores As String() = Nothing) As DataTable
-            Init(store, stores)
-            Return Me.tb
+#Region "Sovathana Function "
 
+#End Region
+
+        Public Function Exec_reader(store As String, stores As List(Of Object)) As SqlDataReader
+
+            If bConnected = False Then
+                connection()
+            End If
+
+            Using command As New SqlCommand(store, connect)
+
+
+
+                Try
+                    command.CommandType = CommandType.StoredProcedure
+                    Dim param As SqlParameter
+                    If stores Is Nothing Then
+                    Else
+                        With command
+                            ' .Parameters.Add("@ID", SqlDbType.SmallInt).Direction = ParameterDirection.ReturnValue
+                            For i As Integer = 0 To stores.Count - 1
+                                .Parameters.AddWithValue(stores(i), stores(i + 1))
+                                i = i + 1
+                            Next
+                            ' .Parameters.Add("@photo", SqlDbType.VarBinary, -1).Value = ""
+                        End With
+                    End If
+                    Try
+                        rder = command.ExecuteReader()
+                    Catch ex As Exception
+
+                    End Try
+                    Me.rder = rder
+                Catch ex As Exception
+
+                End Try
+            End Using
+            Return Me.rder
+        End Function
+        Public Sub Exec(store As String, stores As List(Of Object), Optional type As String = Nothing)
+            If bConnected = False Then
+                connection()
+            End If
+
+            Using command As New SqlCommand(store, connect)
+                Dim dt As New DataTable()
+                Try
+                    command.CommandType = CommandType.StoredProcedure
+                    Dim param As SqlParameter
+                    Dim stypes As String = type.ToLower()
+
+                    If stypes.Equals("select") Then
+                        If stores Is Nothing Then
+                        Else
+                            With command
+                                ' .Parameters.Add("@ID", SqlDbType.SmallInt).Direction = ParameterDirection.ReturnValue
+                                For i As Integer = 0 To stores.Count - 1
+                                    .Parameters.AddWithValue(stores(i), stores(i + 1))
+                                    i = i + 1
+                                Next
+                                ' .Parameters.Add("@photo", SqlDbType.VarBinary, -1).Value = ""
+                            End With
+                        End If
+                        Try
+                            rder = command.ExecuteReader()
+                            dt.Load(rder)
+                        Catch ex As Exception
+
+                        End Try
+                        Me.tb = dt
+                    End If
+                    If stypes.Equals("insert") OrElse stypes.Equals("update") OrElse stypes.Equals("delete") Then
+                        With command
+                            .Parameters.Add("@ID", SqlDbType.SmallInt).Direction = ParameterDirection.ReturnValue
+                            For i As Integer = 0 To stores.Count - 1
+                                .Parameters.AddWithValue(stores(i), stores(i + 1))
+                                i = i + 1
+                            Next
+                            ' .Parameters.Add("@photo", SqlDbType.VarBinary, -1).Value = ""
+                        End With
+                        Try
+                            affected = command.ExecuteNonQuery()
+                        Catch ex As Exception
+
+                        End Try
+                        affected_rows = affected
+                    End If
+                Catch ex As Exception
+
+                End Try
+
+            End Using
+
+        End Sub
+        Public Function Exec_Perform(store As String, stores As List(Of Object), Optional type As String = Nothing) As Integer
+            Exec(store, stores, "insert")
+            Return affected_rows
+        End Function
+        Public Function Execute(Store As String, stores As List(Of Object), Optional type As String = Nothing) As DataTable
+            Exec(Store, stores, type)
+            Return Me.tb
+        End Function
+        Public Function Execute_getColumns(Store As String, stores As List(Of Object), Optional type As String = Nothing) As List(Of String)
+            Exec(Store, stores, type)
+            Dim column As New List(Of String)()
+
+            Dim i As Integer = 0
+            While System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1) < tb.Rows.Count
+
+
+                column.Add(tb.Rows(i)(0).ToString())
+            End While
+
+            Return column
         End Function
         Public Function Field(table As String, field__1 As String, Optional sonaData As String() = Nothing) As DataTable
             Init(Convert.ToString("SELECT * FROM") & table, sonaData)
@@ -210,8 +430,8 @@ Namespace sqlCms
 
         End Sub
 
-        Public Function nQuery(query As String, Optional sonaData As String() = Nothing) As Integer
-            Init(query, sonaData)
+        Public Function nQuery(query As String, Optional sonaData As String() = Nothing, Optional isBinary As Boolean = False, Optional bin As String = Nothing, Optional filebyte As Byte() = Nothing) As Integer
+            Init(query, sonaData, isBinary, bin, filebyte)
             Return Me.affected_rows
         End Function
 
@@ -251,12 +471,22 @@ Namespace sqlCms
             While System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1) < tb.Rows.Count
 
 
-                column.Add(tb.Rows(i - 1)(0).ToString())
+                column.Add(tb.Rows(i)(0).ToString())
             End While
 
             Return column
         End Function
+        Public Sub RetrievToTextbox(stp As String, lst As List(Of Object), ByVal ParamArray txtbox As List(Of Object)())
+            Exec(stp, lst, "select")
+            If rder.Read() Then
+                For i As Integer = 0 To txtbox.GetUpperBound(0)
+                    txtbox(i) = rder.Item(i - 2)
+                Next
+            End If
+            rder.Close()
+            command.Dispose()
 
+        End Sub
 
         Public Sub Debug([error] As String)
             Console.WriteLine([error] & Convert.ToString("/n/r"))
